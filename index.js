24de1d96a9aeee6cc85f983975f9c4f8d6f3b1d7
elhanAliev <alievelhan90@gmail.com>
2023-08-21 16:46:16 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+alert(" Привет!"); 
+const first = prompt("Как вас зовут?") //  Кайрылниса 
+const sourname = prompt("Как ваша фамилия?") // AMIRASTANOV Elhan 
+alert(sourname + " " + first) // amirastanov elhan 
+const num = prompt("Ваш год рождения") // 2008
+alert("Вам 16 лет") // ответ 
+ 
+ 
+const color1 = prompt(" 🚦Какой цвет светафора 🔴 ") // красный 
+alert( "стой Кайрылниса!") 
+const color2= prompt(" 🚦Какой цвет светафора 🟡") // желтый 
+alert("Подожди Кайрылниса!") 
+const color3 = prompt(" 🚦Какой цвет светафора 🟢") // зеленый 
+alert(" Вот теперь идите смело,пешеходам путь открыт Кайрылниса!") // ответ 
+  
+ 
+var firstNumber, secondNumber;
+firstNumber = +prompt('Ведите число: ');
+secondNumber = parseInt(prompt ('Ведите число: '));
+if(firstNumber > secondNumber ){
+    alert(firstNumber + ' Больше');
+}else{
+    alert(secondNumber + ' Больше');
+}
